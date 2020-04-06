145fc683c57e4eb0d09cec10af72f3a2310878c6
Albertosx <a.seijo@alumnos.upm.es>
2020-04-06 22:45:37 +0200

    Header medio completo

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!-- Pie de página, compartido por todas las páginas. Ya veremos qué ponemos aquí, si es que ponemos algo -->
+  <script src="./js/jquery-3.4.1.min.js"></script>
+  <script src="./js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
--- header.php
+++ header.php
@@ -6,18 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/header.css" rel="stylesheet">
     <title>La Vandería</title>
   </head>
   <body>
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">La Vandería</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand logo-empresa" href="#">
+        <img src="./img/LaVandería Logo.png" width="90" height="auto" class="d-inline-block align-top" alt="">
+        La Vandería
+      </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
+          <ul class="navbar-nav mx-auto">
             <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -34,10 +37,16 @@
         </div>
     </nav>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <!-- Prueba de la parte del título de la página + botón de retroceder -->
+
+    <div class="container-fluid contenedor-titulo-pagina">
+      <div class="fondo-titulo-pagina">
+      </div>
+      <div class="container-fluid contenido-titulo-pagina">
+        <button type="button" class="btn btn-primary ml-2 my-auto boton-retroceder-titulo-pagina">Atrás</button>
+        </button>
+        <div class="mx-auto my-auto texto-titulo-pagina">
+          Título de la página actual
+        </div>
+      </div>
+    </div>
