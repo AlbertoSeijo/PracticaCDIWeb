5f87f0dcc5bfe2a7710c5d5d1f69a0c2b478c7bf
Albertosx <a.seijo@alumnos.upm.es>
2020-04-07 13:59:04 +0200

    Consulta de ejemplo

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
 include './header.php';?>
+
+
+
 <?php
-$nombrePagina = 'Aquí pones el titulo';
-include './cabeceraContenido.php';?>
+    define('SERVIDOR_BD', 'localhost:3306');
+    define('USUARIO_BD', 'webtintoreria');
+    define('CONTRASENA_BD', 'lavanderia');
+    define('NOMBRE_BD', 'tintoreria');
+    $db = mysqli_connect(SERVIDOR_BD,USUARIO_BD,CONTRASENA_BD,NOMBRE_BD);
+    session_start();
+
+    $sql = "SELECT * FROM tipopedido";
+    $result = mysqli_query($db,$sql);
+    while($row = $result->fetch_assoc()) {
+            echo "idTipoPedido: " . $row["idTipoPedido"]. " - nombreTipoPedido: " . $row["nombreTipoPedido"]. " Precio:" . $row["precio"]. "<br>";
+        }
+?>
+
+
+
+
 <?php
 include './footer.php';
 ?>
