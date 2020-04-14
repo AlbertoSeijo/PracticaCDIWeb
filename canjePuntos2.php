7d8d65b9f9ac60acbcd9f294d63b405bd179667f
JmBaroja <jm.baroja@alumnos.upm.es>
2020-04-14 17:06:45 +0200

    Canjeo de puntos mas o menos

--- canjePuntos2.php
+++ canjePuntos2.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION['sesionIniciada']) || $_SESSION['tipoCuentaSesión'] != "Enc
 }
 ?>
 <?php
-$nombrePagina = "canjePuntos";
+$nombrePagina = "Canjeo de regalos y descuentos";
 include './cabeceraContenido.php';
 ?>
 
@@ -14,23 +14,93 @@ include './cabeceraContenido.php';
 
 
 
-<div class="container">
+<div class="container-fluid">
   <div class="row">
     <div class="col">
       <div class="text-center">
-      <img src="..." alt="..." class="rounded">
-      <p>1234 - 5678 - 9012</p>
-      <p  align="left">Puntos:</p> <p align="rigth">2350</p>
+      <img src="./img/tarjetaPuntos.svg" width="300" height="auto" alt="" class="rounded">
+      <h4><b>1234 - 5678 - 9012</b></h4>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h6><b>Puntos:</b></h6>
+      </div>
+      <div class="col-rigth">
+        <h6><b>2350</b></h6>
+      </div>
+     </div>
+     <div class="text-center">
+     <button type="button" class="btn btn-info"><h4>Continuar con el pedido</h4></button>
+   </div>
+   </div>
+
+
+    <div class="col">
+      <h3>DESCUENTOS</h3>
+      <div class="card bg-primary text-white">
+        <div class="card-body">
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      </div>
       </div>
     </div>
 
-    <div class="col">
-      <div class="row">
 
+    <div class="col">
+      <h3>REGALOS</h3>
+      <div class="card bg-primary text-white">
+        <div class="card-body">
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <img src="./img/tarjetaPuntos.svg" width="50" height="auto" alt="" class="rounded">33% de descuento
+        </div>
+      </div>
+      </div>
+      </div>
     </div>
-    <div class="col">
-      <div class="row">
-
     </div>
   </div>
 </div>
