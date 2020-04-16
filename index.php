e37067e9a55f102b86ff657557c2b51266a56c6d
JmBaroja <jm.baroja@alumnos.upm.es>
2020-04-16 13:22:23 +0200

    Index-nologueado

--- index.php
+++ index.php
@@ -8,7 +8,39 @@ include './cabeceraContenido.php';
 <div class="web-content container-fluid">
 <?php
 if(!isset($_SESSION['sesionIniciada'])){
-  echo 'Página principal, sin sesión iniciada';
+  echo '
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
+
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./img/backgroundImage0.jpg" width=100% height=700px alt="">
+      </div>
+      <div class="carousel-item">
+        <img src="./img/backgroundImage1.jpg" width=100% height=700px alt="">
+      </div>
+      <div class="carousel-item">
+        <img src="./img/backgroundImage2.jpg" width=100% height=700px alt="">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+
+  </div>
+  ';
 } else {
   if($_SESSION['tipoCuentaSesión'] == "Cliente"){
     echo '
