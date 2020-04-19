fe0d2658c2afc8ad225455b5199ccde3789cfb52
JmBaroja <jm.baroja@alumnos.upm.es>
2020-04-19 11:56:19 +0200

    resumenPedido.php

---
+++ resumenPedido.php
@@ -0,0 +1,20 @@
+<?php
+include './header.php';
+if(!isset($_SESSION['sesionIniciada']) || $_SESSION['tipoCuentaSesión'] != "Encargado"){
+  header("Location: ./");
+}
+?>
+<link href="./css/peticionLimpieza.css" rel="stylesheet">
+<?php
+$nombrePagina = "Resumen del Pedido";
+include './cabeceraContenido.php';
+?>
+
+
+
+
+
+
+<?php
+include './footer.php';
+?>
