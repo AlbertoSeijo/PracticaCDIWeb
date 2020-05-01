b67473312c895543b6c3b633711727ff4677b029
Albertosx <a.seijo@alumnos.upm.es>
2020-05-01 14:11:23 +0200

    v2

--- detallesPedido.php
+++ detallesPedido.php
@@ -298,6 +298,9 @@ if (isset($_SESSION['sesionIniciada'])){
             <div class="fechas row w-100" style="margin-top:30px;">
               <div class="contenedorFechaNombre col-5">Inicio pedido:</div> <div class="contenedorFechaDato col-7" id="fechaInicioPedido"></div>
             </div>
+            <div class="fechas row w-100" style="display: none;">
+              <div class="contenedorFechaNombre col-5">Fin pedido:</div> <div class="contenedorFechaDato col-7" id="fechaFinPedido"></div>
+            </div>
             <div class="fechas row w-100">
               <div class="contenedorFechaNombre col-5">Inicio etapa:</div> <div class="contenedorFechaDato col-7" id="fechaInicioEtapa"></div>
             </div>
